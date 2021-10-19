9f695037047230768eb6fe69b8eef379ca933bc5
PrivateGER <privateger@privateger.me>
2021-10-19 09:46:49 +0200

    Add static resources

--- PageHandlers/Static.go
+++ PageHandlers/Static.go
@@ -1,13 +1,3 @@
 package PageHandlers
 
-import (
-	"embed"
-	"net/http"
-)
 
-//go:embed static
-var staticEmbedFS embed.FS
-
-func Static(writer http.ResponseWriter, request *http.Request) {
-	http.FileServer(http.FS(staticEmbedFS))
-}
